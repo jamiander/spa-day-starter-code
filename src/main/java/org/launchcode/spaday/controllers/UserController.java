ca4ea6fff8c5e79f7e357d08226bca7c00f89635
Jamie Andersen <jamieandersen29@gmail.com>
2020-02-13 15:04:37 -0600

    password verification not quite working

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,18 +1,38 @@
 package org.launchcode.spaday.controllers;
 
 
+import org.launchcode.spaday.models.Client;
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+
 public class UserController {
 
-    @GetMapping("/user/add")
-    public String displayAddUserForm() {
-        return "add.html";
+    @GetMapping ("user/add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute("title", "User Application");
+        return "user/add";
     }
 
+    @PostMapping ("user/add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        user = new User(user.getUsername(), user.getEmail(), user.getPassword());
+        model.addAttribute("user", user);
+        model.addAttribute("verify", verify);
 
+        /*model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        model.addAttribute("password", user.getPassword());*/
+
+        /*if (verify != user.getPassword()) {
+            return "user/add";
+    }*/
+        return "user/index";
+    }
 }
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,17 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+
 public class User {
     private String username;
     private String email;
     private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     public String getUsername() {
         return username;
